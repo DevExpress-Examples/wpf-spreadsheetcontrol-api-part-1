d45fa62a8a3eb8ec847d661cb7a89d0eda81849d
dxMaria <mariya.nikulina@devexpress.com>
2023-01-26 14:12:05 +0400

    Update project settings

--- VB/SpreadsheetControl_WPF_API/My Project/AssemblyInfo.vb
+++ VB/SpreadsheetControl_WPF_API/My Project/AssemblyInfo.vb
@@ -7,14 +7,6 @@ Imports System.Windows
 ' General Information about an assembly is controlled through the following 
 ' set of attributes. Change these attribute values to modify the information
 ' associated with an assembly.
-<Assembly: AssemblyTitle("SpreadsheetControl_WPF_API")>
-<Assembly: AssemblyDescription("")>
-<Assembly: AssemblyConfiguration("")>
-<Assembly: AssemblyCompany("")>
-<Assembly: AssemblyProduct("SpreadsheetControl_WPF_API")>
-<Assembly: AssemblyCopyright("Copyright ©  2013")>
-<Assembly: AssemblyTrademark("")>
-<Assembly: AssemblyCulture("")>
 
 ' Setting ComVisible to false makes the types in this assembly not visible 
 ' to COM components.  If you need to access a type in this assembly from 
@@ -48,5 +40,4 @@ Imports System.Windows
 ' You can specify all the values or you can default the Build and Revision Numbers 
 ' by using the '*' as shown below:
 ' [assembly: AssemblyVersion("1.0.*")]
-<Assembly: AssemblyVersion("1.0.0.0")>
-<Assembly: AssemblyFileVersion("1.0.0.0")>
+
